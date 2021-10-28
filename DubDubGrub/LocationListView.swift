2d9852c0c655171f7f978b1408adc891830d28a9
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-28 09:54:08 +0200

    Location List View

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List View")
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Test LocationName")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            
+                            HStack {
+                                AvertarView()
+                                AvertarView()
+                                AvertarView()
+                                AvertarView()
+                                AvertarView()
+                            }
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("Grub Spots")
+        }
+        
+    }
+}
+
+struct AvertarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
 
