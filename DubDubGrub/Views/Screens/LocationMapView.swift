e1e96fb36c09f30b70af5743ec35abf85865bb1f
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-29 10:56:58 +0200

    AlertItem Setup

--- DubDubGrub/Views/Screens/LocationMapView.swift
+++ DubDubGrub/Views/Screens/LocationMapView.swift
@@ -12,6 +12,8 @@ struct LocationMapView: View {
     
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     
+    @State private var alertItem: AlertItem?
+    
     var body: some View {
         ZStack {
             Map(coordinateRegion: $region)
@@ -22,14 +24,17 @@ struct LocationMapView: View {
                 Spacer()
             }
         }
+        .alert(item: $alertItem) { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        }
         .onAppear {
             CloudKitManager.getLocations { result in
                 switch result {
                     
                 case .success( let locations):
                     print("\(locations)")
-                case .failure( let error):
-                    print(error)
+                case .failure(_):
+                    alertItem = AlertContext.unableToGetLocations
                 }
             }
         }
