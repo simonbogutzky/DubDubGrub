c85fb9ef241ac4aa5a89360d2158f6fd7faa62b1
Simon Bogutzky <simon.bogutzky@gmail.com>
2021-10-30 11:21:36 +0200

    UIImage -> CKAsset Conversion

---
+++ DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -0,0 +1,29 @@
+//
+//  UIImage+Ext.swift
+//  DubDubGrub
+//
+//  Created by Simon Bogutzky on 30.10.21.
+//
+
+import CloudKit
+import UIKit
+
+extension UIImage {
+    func convertToCKAsset() -> CKAsset? {
+        
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        let fileUrl = urlPath.appendingPathComponent("selectedAvatarImage")
+        
+        guard let imageData = jpegData(compressionQuality: 0.25) else { return nil}
+        
+        do {
+            try imageData.write(to: fileUrl)
+            return CKAsset(fileURL: fileUrl)
+        } catch {
+            return nil
+        }
+    }
+}
